9565814aa443e257b45b41025f55f24518f6a1a1
Dennis Reimann <mail@dennisreimann.de>
2021-10-26 20:03:02 +0200

    Remove edit-json-file dependency
    To get rid of this vulnerability in its set-value dependency:
    https://github.com/advisories/GHSA-4jqc-8m5r-9rpr

--- node-file-rev.js
+++ node-file-rev.js
@@ -1,6 +1,5 @@
 const hasha = require('hasha')
 const fs = require('fs')
-const editJsonFile = require('edit-json-file')
 const chalk = require('chalk')
 
 module.exports = (input, flags, cli) => {
@@ -10,29 +9,23 @@ module.exports = (input, flags, cli) => {
     return
   }
   // if argumens are provided
-  input.forEach( filePath => {
-    hasha.fromFile(filePath, { algorithm: 'md5' }).then(hash => {
-      // split filename
-      let fileArray = filePath.split('.')
-      // get file path without extension
-      let filename = fileArray.slice(0, -1).join('.')
-      // get file extension
-      let extension = fileArray.pop()
-      // create hashed filename
-      let newFilename = `${filename}-${hash}.${extension}`
-      // append hash to file
-      fs.rename(filePath, newFilename, (err) => {
-        if (err) {
-          throw err
-        }
-        // If the file doesn't exist, the content will be an empty object by default.
-        let jsonFile = editJsonFile(flags.manifest)
-        // add file version
-        jsonFile.set(filePath.replace(/\./g, '\\.').replace(flags.root,''), newFilename.replace(flags.root,''))
-        // save to json
-        jsonFile.save()
-      })
-    })
+  const data = {}
+  input.forEach(filePath => {
+    const hash = hasha.fromFileSync(filePath, { algorithm: 'md5' })
+    // split filename
+    const fileArray = filePath.split('.')
+    // get file path without extension
+    const filename = fileArray.slice(0, -1).join('.')
+    // get file extension
+    const extension = fileArray.pop()
+    // create hashed filename
+    const newFilename = `${filename}-${hash}.${extension}`
+    // append hash to file
+    fs.renameSync(filePath, newFilename)
+    // add file version
+    const plain = filePath.replace(flags.root, '')
+    data[plain] = newFilename.replace(flags.root, '')
   })
-
+  // save to json
+  fs.writeFileSync(flags.manifest, JSON.stringify(data, null, 2))
 }
